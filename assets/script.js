9833f80af80a361d1b008f76e612d6201c759316
Euri Tanaka <97496667+Seitenmaou@users.noreply.github.com>
2022-02-07 19:00:27 -0800

    Add funcitonality to page, +- buttons, Qty, Ttl shows results

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,57 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
+let yourName = "Euri Tanaka" 
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
-
-// Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+//update page with quantity of each type and total
+const qtyTtl = document.querySelector('#qty-total')
+function updateQuantity(cookieType, cookieAmt)
+{
+    cookieType.textContent = cookieAmt
+    qtyTtl.textContent = gb + cc + sugar
+}
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+//GingerBread
+const qtyGB = document.querySelector('#qty-gb')
+const gbPlusBtn = document.querySelector('#add-gb')
+gbPlusBtn.addEventListener('click', function() {
+    updateQuantity(qtyGB, ++gb)
+})
+const gbMinusBtn = document.querySelector('#minus-gb')
+gbMinusBtn.addEventListener('click', function() {
+    if (gb > 0)
+    {
+        updateQuantity(qtyGB, --gb)
+    }
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+//Chocolate Chip
+const qtyCC = document.querySelector('#qty-cc')
+const ccPlusBtn = document.querySelector('#add-cc')
+ccPlusBtn.addEventListener('click', function() {
+    updateQuantity(qtyCC, ++cc)
+})
+const ccMinusBtn = document.querySelector('#minus-cc')
+ccMinusBtn.addEventListener('click', function() {
+    if (cc > 0)
+    {
+        updateQuantity(qtyCC, --cc)
+    }
+})
+
+//Sugar Sprinkle
+const qtySugar = document.querySelector('#qty-sugar')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+sugarPlusBtn.addEventListener('click', function() {
+    updateQuantity(qtySugar, ++sugar)
+})
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+sugarMinusBtn.addEventListener('click', function() {
+    if (sugar > 0)
+    {
+        updateQuantity(qtySugar, --sugar)
+    }
+})
